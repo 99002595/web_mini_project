03e7c15dfe70a3c4676d0a8654ec7b7cba7020ef
99002595 <71485958+99002595@users.noreply.github.com>
2020-11-11 07:34:08 +0530

    Update client.js

--- client.js
+++ client.js
@@ -3,27 +3,26 @@ const app=express();
 app.use(express.static("images"));
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/Home.html");
-})
+});
 app.get("/About.html", (req, res)=>{
     res.sendFile(__dirname + "/About.html");
-})
+});
 app.get("/View.html", (req, res)=>{
     res.sendFile(__dirname + "/View.html");
-})
+});
 app.get("/contact.html", (req, res)=>{
     res.sendFile(__dirname + "/contact.html");
-})
+});
 app.get("/EventOrganizer.html", (req, res)=>{
     res.sendFile(__dirname + "/EventOrganizer.html");
-})
+});
 
 app.get("/organizer_CRED.html", (req, res)=>{
     res.sendFile(__dirname + "/organizer_CRED.html");
-})
+});
 app.get("/user.html", (req, res)=>{
     res.sendFile(__dirname + "/user.html");
-})
-
+});
 app.listen(3333, ()=>{
     console.log("Client App running at 3333");
 })
